0eebe59297aaade550a92424cf02dde44e3c726d
bartugkeskn <bartukeskin1999@gmail.com>
2025-01-21 14:38:15 +0300

    Add project files.

---
+++ SignalR.EntityLayer/Entities/Feature.cs
@@ -0,0 +1,13 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class Feature
+    {
+        public int FeatureID { get; set; }
+        public int Title1 { get; set; }
+        public string Description1 { get; set; }
+        public int Title2 { get; set; }
+        public string Description2 { get; set; }
+        public int Title3 { get; set; }
+        public string Description3 { get; set; }
+    }
+}
