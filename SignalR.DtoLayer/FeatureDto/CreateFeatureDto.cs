e6f79c51ddf439b4a618e9ef373c2e93d3f3ac42
bartugkeskn <bartukeskin1999@gmail.com>
2025-01-22 14:24:28 +0300

    ...

---
+++ SignalR.DtoLayer/FeatureDto/CreateFeatureDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DtoLayer.FeatureDto
+{
+    public class CreateFeatureDto
+    {
+        public int Title1 { get; set; }
+        public string Description1 { get; set; }
+        public int Title2 { get; set; }
+        public string Description2 { get; set; }
+        public int Title3 { get; set; }
+        public string Description3 { get; set; }
+    }
+}
